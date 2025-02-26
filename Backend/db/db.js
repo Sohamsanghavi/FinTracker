b8c4e95a019a8edceebb6d91c55b9f21b82a6c25
Soham Sanghavi <sanghavisoham02@gmail.com>
2025-02-26 23:53:31 +0530

    deployment

--- Backend/db/db.js
+++ Backend/db/db.js
@@ -2,11 +2,10 @@ const { Pool } = require('pg');
 require('dotenv').config(); // Load environment variables from .env file
 
 const pool = new Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT
+    connectionString: process.env.DB_URL,
+    ssl: {
+        rejectUnauthorized: false, // Required for Supabase
+    },
 });
 
 // pool.on('connect', () => {
